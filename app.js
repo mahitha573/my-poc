f37c535b6afb44883487f870369727633aeb4964
mahitha573 <pamudurthimahitha24@gmail.com>
2025-11-14 09:58:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Happy Childrens Day'));
+app.get('/', (req, res) => res.send('Happy Childrens Day, Let the child in you alive'));
 app.listen(3000, () => console.log('App running on port 3000'));
