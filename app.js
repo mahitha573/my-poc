0bfede284b64667e3366fd10000ba8502012030e
mahitha573 <pamudurthimahitha24@gmail.com>
2025-11-14 09:54:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello from Node.js App!'));
+app.get('/', (req, res) => res.send('Happy Childrens Day'));
 app.listen(3000, () => console.log('App running on port 3000'));
